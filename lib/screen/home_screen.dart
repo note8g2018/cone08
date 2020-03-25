d17fcac2a72533656756a2b15be8cdffc211b1f4
GL553VD <GL553VD@FX553VD-DM585T>
2020-03-25 20:23:11 +0300

    ok1

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:cone08/action/file_action.dart';
 import 'package:cone08/controller/logout_controller.dart';
 import 'package:cone08/model/person_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:web_socket_channel/io.dart';
@@ -20,18 +22,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  File _image;
+  File imageFile;
+  Image image;
+  List<Image> imageList =[];
+  List<Text> ttt = [Text('a'), Text('b'),];
   IOWebSocketChannel channel;
 
   void websocketInit() {
     const String url = 'ws://192.168.1.100:8888/picw';
     channel = IOWebSocketChannel.connect(url);
+    channel.stream.listen(
+            (data){
+          setState(() {
+            imageList = List.from(imageList)..add(Image.memory(base64Decode(data)));
+
+            //ttt =List.from(ttt)..add(Text('c'));
+            print('fff');
+          });
+
+        }
+    );
   }
 
   @override
   void initState() {
+    websocketInit();
     // TODO: implement initState
-    print(widget.personClient.toJson());
     super.initState();
   }
 
@@ -42,14 +58,35 @@ class _HomeState extends State<Home> {
   }
 
   Future getImage() async {
-    final File image = await ImagePicker.pickImage(source: ImageSource.camera);
-    channel.sink.add(image);
-    Image kk = Image.file(_image);
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+    final List<int> bytes = imageFile.readAsBytesSync();
+    final String imageS = base64Encode(bytes);
+    final List<int> bytes2 = base64Decode(imageS);
+    final Map<String, dynamic> dataJson =
+    {
+      "userName": widget.personClient.userName,
+      "pic": imageS,
+      "method": "add",
+    };
+
+    channel.sink.add(jsonEncode(dataJson));
+
 
     setState(() {
-      _image = image;
+      image = Image.memory(bytes2);
     });
   }
+
+//  Widget getImageList()
+//  {
+//    setState(() {
+//      for(var ii in imageList)
+//      {
+//        return ii;
+//      }
+//    });
+//
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,9 +95,26 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: IconButton(icon: Icon(Icons.person), onPressed: () => logOut()),
       ),
-      body: Center(
-        child: _image == null ? Text('No image selected.') : Image.file(_image),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: ListView(
+              children: imageList.isEmpty? <Widget>[Text('ggg')] : imageList,
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: image == null ? Text('No image selected.') : image,
+          ),
+        ],
       ),
+
+//          Center(
+//            child:
+//                image == null ? Text('No image selected.') : image,
+//          ),
+ //     ],),
       floatingActionButton: FloatingActionButton(
         onPressed: getImage,
         tooltip: 'Pick Image',
